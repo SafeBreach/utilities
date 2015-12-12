8f4bcf4d2e271b7d1c983318db616af6a7aac15f
Gadi Lifshitz <gadi@safebreach.com>
2015-12-12 17:28:14 +0200

    a little cleanup

--- src/stackframes.js
+++ src/stackframes.js
@@ -1,9 +1,9 @@
 'use strict';
+// return the relevant call stack to allow for use while logging, for example
 function stackFrames(depth) {
     depth = (typeof depth !== 'undefined') ? depth : 2;
     var err = new Error();
     var frame = err.stack.split('\n')[depth];
-   // console.log(frame);
     var p = frame.match(/at\s*([A-Za-z0-9._-]*).*\/([A-Za-z0-9._-]+\.\w*:\d*:\d*)/);
     // let's be a little protective...
     if (p === null) {
