cb5f1595aa6b7cb8367909a46e04b3f84ee03495
Gadi Lifshitz <gadi@safebreach.com>
2015-12-14 14:30:20 +0200

    updated license and minor test modifications

--- test/test-stackframes.js
+++ test/test-stackframes.js
@@ -6,9 +6,10 @@ var stackframes = require('../src/stackframes.js');
 
 describe('StackFrames', function() {
   describe('#stackFrames(depth)', function () {
-    it('should return function name correctly', function () {
+    it('should return filenmae (and line) name correctly', function () {
     	var currentFilename = module.filename.slice(__filename.lastIndexOf(path.sep)+1, module.filename.length);
-    	stackframes.stackFrames(2)[2].should.startWith(currentFilename);
+    	// The element at position 2 holds the current filename (and line in the code, formatted filename:line)
+    	stackframes.stackFrames()[2].should.startWith(currentFilename+":");
     });
   });
 });
